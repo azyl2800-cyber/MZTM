f3bdc181b7d4c462adb6af3b29bc019a23dc6086
azyl2800-cyber <azyl2800@gmail.com>
2026-09-02 02:06:34 +0200

    Upload 254 files via GitHub Uploader by Mubashir Hassan

---
+++ src/app/(store)/store/[product]/page.tsx
@@ -0,0 +1,9 @@
+﻿// TODO: IMPLEMENT PRODUCT DETAIL PAGE (FUTURE)
+
+interface ProductPageProps {
+  params: { product: string }
+}
+
+export default function ProductPage({ params }: ProductPageProps) {
+  return <div>Product: {params.product}</div>
+}
