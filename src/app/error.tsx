1abd69a5ce19615f925c341f1226097bd1c4ca5a
azyl2800-cyber <azyl2800@gmail.com>
2026-09-02 02:06:25 +0200

    Upload 254 files via GitHub Uploader by Mubashir Hassan

---
+++ src/app/error.tsx
@@ -0,0 +1,21 @@
+﻿import { Button } from '@/components/ui/Button'
+
+'use client'
+
+// TODO: IMPLEMENT GLOBAL ERROR
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error
+  reset: () => void
+}) {
+  return (
+    <div>
+      <h1>Coś poszło nie tak</h1>
+      <button onClick={reset}>Spróbuj ponownie</button>
+    </div>
+  )
+}
+
