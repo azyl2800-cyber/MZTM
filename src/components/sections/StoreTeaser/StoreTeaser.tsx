636dfbe1127acb9f3909b1f646f2dc88e4c07af5
azyl2800-cyber <azyl2800@gmail.com>
2026-09-02 02:04:24 +0200

    Upload 254 files via GitHub Uploader by Mubashir Hassan

---
+++ src/components/sections/StoreTeaser/StoreTeaser.tsx
@@ -0,0 +1,10 @@
+﻿'use client'
+
+import { Section } from '@/components/ui/Section'
+
+// TODO: IMPLEMENT StoreTeaser SECTION
+export const StoreTeaser = () => {
+  return <section>StoreTeaser</section>
+}
+
+
