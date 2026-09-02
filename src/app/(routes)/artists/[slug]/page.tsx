7f6528b5f32844aa0ff50960bd19e2c8e613de27
azyl2800-cyber <azyl2800@gmail.com>
2026-09-02 02:06:52 +0200

    Upload 254 files via GitHub Uploader by Mubashir Hassan

---
+++ src/app/(routes)/artists/[slug]/page.tsx
@@ -0,0 +1,9 @@
+﻿// TODO: IMPLEMENT ARTIST DETAIL PAGE
+
+interface ArtistPageProps {
+  params: { slug: string }
+}
+
+export default function ArtistPage({ params }: ArtistPageProps) {
+  return <div>Artist: {params.slug}</div>
+}
