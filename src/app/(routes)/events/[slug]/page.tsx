14411f47e983bba10d325aa9ffafd2cd4fdf2772
azyl2800-cyber <azyl2800@gmail.com>
2026-09-02 02:06:46 +0200

    Upload 254 files via GitHub Uploader by Mubashir Hassan

---
+++ src/app/(routes)/events/[slug]/page.tsx
@@ -0,0 +1,9 @@
+﻿// TODO: IMPLEMENT EVENT DETAIL PAGE
+
+interface EventPageProps {
+  params: { slug: string }
+}
+
+export default function EventPage({ params }: EventPageProps) {
+  return <div>Event: {params.slug}</div>
+}
