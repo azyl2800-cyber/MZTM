0d7b7bdb708876d13c56c876f300a7479b594674
azyl2800-cyber <azyl2800@gmail.com>
2026-09-02 02:02:18 +0200

    Upload 254 files via GitHub Uploader by Mubashir Hassan

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+﻿import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  experimental: {
+    typedRoutes: true,
+  },
+  images: {
+    domains: ['localhost'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  swcMinify: true,
+  trailingSlash: false,
+}
+
+export default nextConfig
