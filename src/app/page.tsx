27e9c9074b5b03273b1476d9c9855d96b08f1daf
azyl2800-cyber <azyl2800@gmail.com>
2026-09-02 02:06:31 +0200

    Upload 254 files via GitHub Uploader by Mubashir Hassan

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+﻿'use client'
+
+import { Header } from '@/components/layout/Header'
+import { Hero } from '@/components/sections/Hero'
+import { About } from '@/components/sections/About'
+import { MusicCarousel } from '@/components/sections/MusicCarousel'
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen bg-dark-100">
+      <Header />
+      <Hero />
+      <About />
+      <MusicCarousel />
+    </main>
+  )
+}
