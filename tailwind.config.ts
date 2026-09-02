a1653b164f64dfc5a7bd33fa74dcaa87b93f60e1
azyl2800-cyber <azyl2800@gmail.com>
2026-09-02 02:02:24 +0200

    Upload 254 files via GitHub Uploader by Mubashir Hassan

---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+﻿import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primary: {
+          50: '#EDE9FE',
+          100: '#C084FC',
+          200: '#A855F7',
+          300: '#8B5CF6',
+          400: '#6D28D9',
+          500: '#241A38',
+        },
+        dark: {
+          100: '#0F1016',
+          200: '#171522',
+        },
+      },
+      fontFamily: {
+        sans: ['var(--font-geist-sans)'],
+        mono: ['var(--font-geist-mono)'],
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
